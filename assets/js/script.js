2b94f1f7808c3aa468388dd622624c053c1bc340
carl.dunne <developerdunne@gmail.com>
2023-10-20 14:46:26 +0100

    Added random numbers for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-function rungame(){
+document.addEventListener('DOMContentLoaded'), function(){
+    let buttons = document.getElementsByTagName('button');
+    for (let button of buttons) {
+        button.addEventListener('click', function(){
+            if (this.getAttribute(data-type)==='submit') {
+                alert('You clicked submit');
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`you clicked' ${gameType}`);
+            }
+        })
+    }
+})
 
+function rungame(){
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
