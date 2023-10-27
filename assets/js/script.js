d20ede33157034188916df84106fc7f2e1b38b0c
carl.dunne <developerdunne@gmail.com>
2023-10-27 09:57:12 +0100

    Added addition random values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,47 @@
-document.addEventListener('DOMContentLoaded'), function(){
-    let buttons = document.getElementsByTagName('button');
-    for (let button of buttons) {
-        button.addEventListener('click', function(){
-            if (this.getAttribute(data-type)==='submit') {
-                alert('You clicked submit');
-            } else {
-                let gameType = this.getAttribute('data-type');
-                alert(`you clicked' ${gameType}`);
-            }
-        })
-    }
-})
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
 
-function rungame(){
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        runGame(gameType);
+      }
+    });
+  }
+
+  runGame("addition");
+});
+
+function runGame(gameType) {
+  // Creates two random numbers between 1 and 25
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
 }
 
-function checkAnswer() {
+function checkAnswer() {}
 
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "+";
 }
 
-function CalculateCorrextAnswer() {
+function displaySubtractQuestion() {}
 
-}
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer(){
-
-}
-
-function displayAdditionQuestion(){
-
-}
-
-function displaySubtractQuestion(){
-
-}
-
-function displayMultiplQuestion(){
-
-}
-
-function displayDivisionQuestion(){
-
-}
+function displayMultiplyQuestion() {}
